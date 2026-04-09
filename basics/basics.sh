a689e4997e1a957d94936bf61fbfd0d26f26d6a3
Tumtum <56156499+Byte-tumtum@users.noreply.github.com>
2026-04-09 21:59:44 +0200

    First commit

---
+++ basics/basics.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# You will implement this exercise with a shell script that contains some awk commands.
+# In subsequent exercises, the solution file will contains only awk code.
+#
+# You have 4 tasks to solve.
+# Replace the `{exit}` action with awk code to solve the task.
+
+## task 1
+awk 'BEGIN {FS=":"} {print $1}' ./passwd
+
+## task 2
+awk '{print NR}' ./passwd
+
+## task 3
+
+awk '
+    # The function definition (stays outside the main loop)
+    function startsWith(text, prefix) {
+        return text ~ "^"prefix
+    }
+    {
+      FS=":"
+      $0=$0; 
+      if (!startsWith($6, "/home") && !startsWith($6, "/root")) {print $0}
+    }
+' ./passwd
+
+## task 4
+awk '
+    function startsWith(text, prefix) {
+        return text ~ "^"prefix
+    }
+
+    {
+      FS=":"
+      $0=$0;
+      if ((startsWith($6, "/home") || startsWith($6, "/root")) && startsWith($7, "/bin/bash")) {print $0}
+    }
+' ./passwd
---
+++ hello-world/hello-world.awk
@@ -0,0 +1,2 @@
+#!/usr/bin/env -S gawk -f
+BEGIN {print "Hello, World!"}
